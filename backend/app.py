f47808c98ab3ba1cab8636e980ce573ebdd60c9a
Leonhard Horstmeyer <leo.h@hotmail.de>
2023-09-17 16:06:48 +0200

    adapted the dockerfiles and app to include ipfs support

--- backend/app.py
+++ backend/app.py
@@ -3,14 +3,17 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from config.glob import (
     ALLOWED_UPLOAD_EXTENSIONS,
+    IMAGE_UPLOAD_SIZE_LIMIT,
     IMAGE_UPLOAD_FOLDER,
+    IMAGE_THUMB_FOLDER,
     VISION_BUCKET_NAME
 )
 from utils.path import (
     export_credential_path, 
     load_env, 
     create_tables_if_with_db,
-    create_upload_directory_if_not_there)
+    create_directory_if_not_there)
+from utils.products import __get_main_product_set_id
 from db.utils import create_tables
 from db.session import engine, SessionLocal
 from db.mock import mockdb
@@ -21,12 +24,15 @@ from db.models import (
 from vision.query import (
     list_product_sets, 
     list_products_and_product_sets,
+    list_products_in_product_set,
     list_reference_images_for_product)
 from vision.addimage import create_reference_image
 from vision.createset import create_product_set
 from vision.manage import create_product, add_product_to_product_set, remove_product_from_product_set
-from vision.storage import upload_file
+from vision.storage import upload_file, allowed_file
 from vision.similarity import search_products
+from vision.compression import compress_image
+from vision.ipfs import upload_image_to_ipfs
 
 
 
@@ -35,7 +41,11 @@ from vision.similarity import search_products
 load_env()
 export_credential_path()
 create_tables_if_with_db()
-create_upload_directory_if_not_there(folder=IMAGE_UPLOAD_FOLDER)
+# create path to upload folder with os.path.join and abspath
+create_directory_if_not_there(
+    folder=os.path.abspath(os.path.join(os.path.dirname(__file__), IMAGE_UPLOAD_FOLDER)))
+create_directory_if_not_there(
+    folder=os.path.abspath(os.path.join(os.path.dirname(__file__), IMAGE_THUMB_FOLDER)))
 # print('GOOGLE_CREDENTIAL_PATH', os.getenv("GOOGLE_CREDENTIAL_PATH"))
 
 
@@ -43,28 +53,74 @@ create_upload_directory_if_not_there(folder=IMAGE_UPLOAD_FOLDER)
 app = Flask(__name__)
 # CORS(app, origins=["http://localhost:3000", "http://another.domain"])  # This will allow all origins to access your app. It's okay for development but not for production!
 
+routes = {
+    "HOME": {"path":"/"},
+    "UPLOAD": {"path":"/upload"},
+    "PRODUCT_SETS": {"path": '/vision/listProductSets'},
+    "SEARCH_PRODUCTS_FROM_FILE": {"path":"/vision/searchProductsFromFileName", "args":['filename', 'product_set_id']},
+    "TEMP_UPLOAD":{"path":"/tempUpload"}
+}
 
-@app.route('/')
+@app.route(routes["HOME"]["path"])
 def root():
     return jsonify(message="Hello from the Object Minting!")
 
 
-@app.route('/upload', methods=['POST'])
+@app.route(routes["UPLOAD"]["path"], methods=['POST'])
 def upload_file_route():
     result_dict, status = upload_file(
         files=request.files, size=request.content_length)
     return result_dict, status
 
-# @app.route('/vision/listProductSets', methods=['GET'])
 
-@app.route('/vision/listProductSets', methods=['GET'])
+@app.route(routes["TEMP_UPLOAD"]["path"], methods=['POST'])
+def temp_upload_file_route():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+    
+    # check size
+    if int(request.content_length) > IMAGE_UPLOAD_SIZE_LIMIT:
+        return jsonify({"error": f'File size too large: {request.content_length}'}), 400
+    
+    # print the size of the file
+    print(f'File size: {request.content_length}')
+    # source_file_name = os.path.join(os.path.dirname(__file__), 'img', 'SocksOrNot.jpg')
+    filename = os.path.join(IMAGE_UPLOAD_FOLDER, file.filename)
+    file.save(filename)
+    
+    print(f'File name: {filename}')
+
+    allowed_flag = allowed_file(file.filename)
+
+    if not allowed_flag:
+        return jsonify({"error": "File type not allowed"}), 400
+    
+    # save to thumbnails
+    thumbnail = os.path.join(IMAGE_THUMB_FOLDER, 'thumb_' + file.filename)
+    compress_image(filename, thumbnail)
+    # Upload directly to GCS
+    destination_blob_name = file.filename
+    return {"status":"success"}, 200
+
+# @app.route('/vision/listProductSets', methods=['GET'])
+## test write to a file 
+@app.route('/testWrite', methods=['GET'])
+def test_write():
+    with open('testfile.txt', 'w') as f:
+        f.write('This is a test.')
+
+
+@app.route(routes["PRODUCT_SETS"]["path"], methods=['GET'])
 def list_product_sets_route():
     sets, status =  list_product_sets(os.getenv("GOOGLE_CLOUD_PROJECT"), os.getenv("GOOGLE_CLOUD_PROJECT_LOCATION"))
     print('The sets are', sets)
     return sets, status
 
 
-@app.route('/vision/searchProductsFromFileName/<filename>/<product_set_id>', methods=['GET'])
+@app.route(routes["SEARCH_PRODUCTS_FROM_FILE"]["path"] + '/<filename>/<product_set_id>', methods=['GET'])
 def search_products_route(filename, product_set_id):
     # Example usage
     image_path = os.path.abspath(os.path.join(os.path.dirname(__file__),'img', filename))
@@ -76,8 +132,6 @@ def search_products_route(filename, product_set_id):
         product_category="general-v1", 
         image_path=image_path)
     
-    # for result in matched_products:
-    #     print(f"Matched product: {result["product_name"]}, score: {result.score}")
     print('matched products', matched_products)
     
     return jsonify([{"name":prod["product_name"], "score": prod["score"]} for prod in matched_products]), status
@@ -89,6 +143,13 @@ def list_products_route():
     print('The products are', products)
     print('We would like to return the products here')
     return products, status
+
+
+@app.route('/vision/listProductsInProductSet', methods=['GET'])
+def list_products_in_set_route():
+    products, status =  list_products_in_product_set(os.getenv("GOOGLE_CLOUD_PROJECT"), os.getenv("GOOGLE_CLOUD_PROJECT_LOCATION"), __get_main_product_set_id())
+    return products, status
+
 
 @app.route('/vision/reference_images/<product_id>', methods=['GET'])
 def list_reference_images_for_product_route(product_id):
@@ -156,7 +217,10 @@ def create_reference_image_route():
 @app.route('/vision/uploadImageAndSearchSimilarProducts', methods=['POST'])
 def upload_image_and_search_similar_products_route():
     result_dict, status = upload_file(files=request.files, size=request.content_length)
+    print('uploaded image.', status)
     if status == 200:
+        print('trying to find matching products')
+        print('the product set id is', request.form['product_set_id'])
         image_path = os.path.abspath(os.path.join(os.path.dirname(__file__),'img', request.files['file'].filename))
         matched_products, status = search_products(
             project_id=os.getenv("GOOGLE_CLOUD_PROJECT"), 
@@ -164,9 +228,17 @@ def upload_image_and_search_similar_products_route():
             product_set_id=request.form['product_set_id'], 
             product_category="general-v1", 
             image_path=image_path)
-        return jsonify([{"name":prod["product_name"], "score": prod["score"]} for prod in matched_products]), status
+        matched_product_info = [
+            {
+                "name":prod["product_name"], 
+                "display":prod["product_display_name"],
+                "score": round(prod["score"],4)
+            } for prod in matched_products]
+        print('mathed product', matched_product_info)
+        return jsonify(matched_product_info), status
     else:
         status = 500
+        print(result_dict)
         return result_dict, status
 
 
@@ -182,6 +254,39 @@ def upload_image_to_product_route():
     else:
         status = 500
         return result_dict, status
+    
+@app.route('/vision/createProductAndReferenceImages', methods=['POST'])
+def create_product_and_ref_images():
+    ## First create the product
+    product_id = request.json['product_id']
+    product_display_name = request.json['product_display_name']
+    product_category = 'general-v1'
+    product_set_id = request.json['product_set_id']
+    response_dicts = {}
+    response_dict, status = create_product(os.getenv("GOOGLE_CLOUD_PROJECT"), os.getenv("GOOGLE_CLOUD_PROJECT_LOCATION"), product_id, product_display_name, product_category)
+    response_dicts.update({"create_product":response_dict})
+    if status == 200:
+        print('product has been added')
+        response_dict, status = add_product_to_product_set(os.getenv("GOOGLE_CLOUD_PROJECT"), os.getenv("GOOGLE_CLOUD_PROJECT_LOCATION"), product_id, product_set_id)
+    else:
+        print('product has not been added')
+        status = 500
+        return response_dict, status
+    
+    response_dicts.update({"add_product_to_product_set":response_dict})
+    ## add reference images
+    create_ref_images_response = []
+    blob_names = request.json['files']  
+    for blob_name in blob_names:
+        gcs_uri = f"gs://{VISION_BUCKET_NAME}/{blob_name}"
+        response_dict, status = create_reference_image(os.getenv("GOOGLE_CLOUD_PROJECT"), os.getenv("GOOGLE_CLOUD_PROJECT_LOCATION"), product_id, gcs_uri)
+        create_ref_images_response.append(response_dict)
+    response_dicts.update({"create_reference_images":create_ref_images_response})
+    return response_dicts, status
+
+@app.route('/vision/uploadToIPFS', methods=['GET'])
+def upload_to_ipfs_route():
+    return upload_image_to_ipfs('img/carrot2.jpg')
 
 
 @app.route('/createTables', methods=['GET'])
--- backend/config/glob.py
+++ backend/config/glob.py
@@ -2,3 +2,7 @@ ALLOWED_UPLOAD_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 IMAGE_UPLOAD_FOLDER='uploads'
 IMAGE_UPLOAD_SIZE_LIMIT=20000000
 VISION_BUCKET_NAME="art-images-test"
+IMAGE_THUMB_FOLDER='thumbs'
+TEST_PRODUCT_ID='veggies'
+DEV_PRODUCT_ID='socks'
+MAIN_PRODUCT_ID='schmiede'
